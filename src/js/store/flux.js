967ee57e6cda49bda8af822b01fc8ac2c9801447
labs404 <136141267+labs404@users.noreply.github.com>
2023-08-14 23:45:39 +0000

    added address card styling

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,7 +1,15 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			contacts: []
+			contacts: [
+				{
+					"full_name": "Dave Bradley",
+					"email": "dave@gmail.com",
+					"agenda_slug": "my_super_agenda",
+					"address":"47568 NW 34ST, 33434 FL, USA",
+					"phone":"7864445566"
+				}
+			]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -13,7 +21,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/labs404")
-				.then(response => response.json)
+				.then(response => response.json())
 				.then(data => {
 					console.log(data);
 					setStore({contacts: data})})
--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -4,27 +4,56 @@ import { Context } from "../store/appContext";
 const Contact = () => {
     const { store, actions } = useContext(Context);
 
-    // // this bit of code already occurs ad appContext.js useEffect();
-    // useEffect(() => {
-    //     actions.fetchAllContacts();
-    // }, []);
-
     return (
         <>
-        <ul className="list-group">
+            <div className="container-container">
             {store.contacts.map((contact, index) => {
                 return (
-                    <li key={contact.id}>
-                        <div><b>Full Name:</b> {contact.full_name}</div>
-                        <div><b>Email Address:</b> {contact.email}</div>
-                        <div><b>Containing Address Book:</b> {contact.agenda_slug}</div>
-                        <div><b>Street Address:</b> {contact.address}</div>
-                        <div><b>Phone Number:</b> {contact.phone}</div>
-                    </li>
+                    <div key={contact.id} className="contact-container">
+                            <div className="contact-image-container">
+                                <img className="contact-image-photo" src="https://xsgames.co/randomusers/avatar.php?g=male" />
+                            </div>
+                            <div className="contact-details">
+                                <div className="contact-details-name">
+                                    {contact.full_name}
+                                </div>
+                                <div className="contact-details-address">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="contact-details-svg bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                        <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                                    </svg>
+                                    {contact.address}
+                                </div>
+                                <div className="contact-details-phone">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="contact-details-svg bi bi-telephone-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                                    </svg>
+                                    {contact.phone}
+                                </div>
+                                <div className="contact-details-email">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="contact-details-svg bi bi-envelope-fill" viewBox="0 0 16 16">
+                                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                                    </svg>
+                                    {contact.email}
+                                </div>
+                            </div>
+                            <div className="contact-modify-container">
+                                <div className="contact-modify-edit">
+{/* need to make an edit button with functionality here */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="contact-modify-svg bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                                    </svg>
+                                </div>
+                                <div className="contact-modify-delete">
+{/* need to make a delete button with functionality here */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="contact-modify-svg bi bi-trash-fill" viewBox="0 0 16 16">
+                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                                    </svg>
+                                </div>
+                            </div>
+                    </div>
                 )
             })}
-        </ul>
-        <h1>Hello from Contact.js</h1>
+            </div>
         </>
     );
 };
