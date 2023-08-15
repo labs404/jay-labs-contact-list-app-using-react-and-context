01229812f6d4a480e03261357448d0c52e8c88d4
labs404 <136141267+labs404@users.noreply.github.com>
2023-08-15 00:23:24 +0000

    added delete functionality

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Got to Contacts</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-danger">Check the Context in action</button>
+					<button className="btn btn-primary">Add New Contact</button>
 				</Link>
 			</div>
 		</nav>
