f54ae7b20221d324aba893f1d6df4f4052c25a24
Jay Labs <136141267+labs404@users.noreply.github.com>
2024-04-25 01:08:22 +0000

    updated readme with API documentation

--- src/js/layout.js
+++ src/js/layout.js
@@ -17,12 +17,13 @@ import SingleContact from "./views/singlecontact";
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	
+
 	// const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
+			{/* <BrowserRouter basename={basename}> */}
+			<BrowserRouter>
 				<ScrollToTop>
 					<Navbar />
 					<Routes>
